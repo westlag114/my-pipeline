f26b3527dfa9ce478c66e5f60daa034be5a4c54b
westlag114 <fsww2353@gmail.com>
2023-05-07 22:41:27 +0900

    fix auth

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -13,7 +13,11 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "MyPipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("westlag114/my-pipeline", "main"),
+        input: CodePipelineSource.gitHub("westlag114/my-pipeline", "main", {
+          authentication: cdk.SecretValue.secretsManager(
+            "github-access-token-secret"
+          ),
+        }),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
