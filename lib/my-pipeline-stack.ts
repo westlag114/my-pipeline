f5dd3f93f822b38a053ff79edf03c90261e25745
westlag114 <fsww2353@gmail.com>
2023-05-07 21:57:53 +0900

    fix reponame

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -13,7 +13,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "MyPipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("OWNER/REPO", "main"),
+        input: CodePipelineSource.gitHub("westlag114/my-pipeline", "main"),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
