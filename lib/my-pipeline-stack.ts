968fae1406cdeaf20f5ba0a5e72b55c193e014a5
westlag114 <fsww2353@gmail.com>
2023-05-07 21:54:49 +0900

    fix

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("OWNER/REPO", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
